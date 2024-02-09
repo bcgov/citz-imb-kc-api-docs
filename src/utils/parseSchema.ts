11c2e4f725219505ef74a03dfb387e2b2aead484
BradyMitch <braden.jr.mitch@gmail.com>
2024-02-09 14:45:17 -0800

    Fix

--- src/utils/parseSchema.ts
+++ src/utils/parseSchema.ts
@@ -25,16 +25,25 @@ export const parseSchema = (
   // If queryParams is empty but schemaName and schemaPath are provided,
   // parse the entire schema to extract all query parameters.
   if (Object.keys(query.params).length === 0 && schema) {
-    // Assuming the schema is an object with properties, each representing a query parameter
-    const schemaProperties = Object.keys(schema);
-    schemaProperties.forEach((param) => {
-      const properties = parseSchemaProperty(schema, param, customSchemas);
-      if (properties) params[param] = properties;
-      else {
-        // Default to string type if the property type cannot be determined
-        params[param] = { required: true, type: "string" };
-      }
-    });
+    // Pattern to match the content inside z.object({...})
+    const schemaPattern = /z\.object\(\{([\s\S]*?)\}\)/;
+    const match = schemaPattern.exec(schemaFileContent);
+
+    if (match) {
+      // Extract the properties string from the match
+      const propertiesString = match[1];
+
+      // Split the string by commas not inside quotes to get individual properties
+      const schemaProperties = propertiesString.split(
+        /,(?=(?:[^"]*"[^"]*")*[^"]*$)/
+      );
+
+      schemaProperties.forEach((param) => {
+        const properties = parseSchemaProperty(schema, param, customSchemas);
+        if (properties) params[param] = properties;
+        else params[param] = { required: true, type: "string" };
+      });
+    }
   } else {
     // Handling provided queryParams
     Object.keys(query.params).forEach((param) => {
