72824c8117336752cef5aac9117e7eb1233662e9
BradyMitch <braden.jr.mitch@gmail.com>
2024-02-08 16:49:58 -0800

    Fix scripts

--- src/static/script.js
+++ src/static/script.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
   var executeButtons = document.querySelectorAll(".execute-button");
+  var expandButtons = document.querySelectorAll(".expand-arrow-button");
+
+  expandButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var detailsId = this.getAttribute("data-index");
+      var details = document.getElementById("details-" + detailsId);
+      var svgs = this.querySelectorAll(".arrow-svg");
+
+      // Toggle details visibility
+      var isDisplayed = details.style.display === "block";
+      details.style.display = isDisplayed ? "none" : "block";
+
+      // Switch SVG visibility
+      svgs[0].style.display = isDisplayed ? "block" : "none";
+      svgs[1].style.display = isDisplayed ? "none" : "block";
+    });
+  });
 
   executeButtons.forEach(function (button) {
     button.addEventListener("click", function () {
