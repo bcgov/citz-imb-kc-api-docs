f589a54da1860f574301e665e376a89a22585fb5
BradyMitch <braden.jr.mitch@gmail.com>
2024-02-08 23:53:43 -0800

    Fix

--- src/static/script.js
+++ src/static/script.js
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const svgs = this.querySelectorAll(".module-arrow-svg");
 
       // Toggle details visibility
-      let isDisplayed = details.style.display === "block";
+      let isDisplayed = details.style.display === "none";
       details.style.display = isDisplayed ? "none" : "block";
 
       // Switch SVG visibility
