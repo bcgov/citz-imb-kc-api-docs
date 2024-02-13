4f4d9c3d0e5fcc776b58e35ea7e4c48d3ad2ffb1
BradyMitch <braden.jr.mitch@gmail.com>
2024-02-13 15:18:49 -0800

    Minor: Updated commit-msg hook

--- scripts/pre-commit-version-change.mjs
+++ scripts/pre-commit-version-change.mjs
@@ -1,10 +1,8 @@
-import fs from "fs";
 import { execSync } from "child_process";
 import { resolve } from "path";
 
 // Check commit message for version change type
-const checkCommitMessageForVersionChange = (commitMsgPath) => {
-  const commitMsg = fs.readFileSync(commitMsgPath, "utf8");
+const checkCommitMessageForVersionChange = (commitMsg) => {
   const versionChangeRegex = /^(patch|minor|major)\b/i;
 
   const matchResult = commitMsg.match(versionChangeRegex);
@@ -29,15 +27,15 @@ const executeBumpVersion = (versionChange) => {
 // Main function to run the pre-commit hook logic
 const main = () => {
   // The path to the temporary file that contains the commit message is passed by Git to the pre-commit hook script
-  const commitMsgPath = process.argv[2];
+  const commitMsg = process.argv[2];
 
-  if (!commitMsgPath) {
-    console.error("Error: Commit message file path not provided.");
+  if (!commitMsg) {
+    console.error("Error: Commit message not provided.");
     process.exit(1);
   }
 
   try {
-    const versionChange = checkCommitMessageForVersionChange(commitMsgPath);
+    const versionChange = checkCommitMessageForVersionChange(commitMsg);
     executeBumpVersion(versionChange);
   } catch (error) {
     console.error(`Error: ${error.message}`);
